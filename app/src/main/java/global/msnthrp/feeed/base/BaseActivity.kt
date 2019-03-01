0ca84f4b3c94a50a8dd8eb38daf0034bb857bd82
twoeightnine <twoeightnine@list.ru>
2019-03-01 21:42:10 +0400

    remove exception handler

--- app/src/main/java/global/msnthrp/feeed/base/BaseActivity.kt
+++ app/src/main/java/global/msnthrp/feeed/base/BaseActivity.kt
@@ -10,6 +10,7 @@ open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.setDefaultUncaughtExceptionHandler(ExceptionHandler(this))
+        // i cant use it while sending reports is not working
+//        Thread.setDefaultUncaughtExceptionHandler(ExceptionHandler(this))
     }
 }
